3f74cbfe143d4bcdc14aca8cd97499d7aec4c0a9
Nicholas Amor <nick.amor@gmail.com>
2015-04-27 18:58:10 +1000

    import aws sdk

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<?php echo "Hello World" ?>
+<?php
+require 'aws/aws-autoloader.php'; 
+use Aws\DynamoDb\DynamoDbClient;
+
+/*
+$client = DynamoDbClient::factory(array(
+	'profile' => 'dbapp-profile',
+	'region' => 'ap-southeast-2'
+	));
+	*/
+$client = DynamoDbClient::factory();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<body>
+	<div id="container">
+	<?php echo "Hello World" ?>
+	</div>
+</body>
+</html>
+
